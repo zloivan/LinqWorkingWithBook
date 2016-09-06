1ca78c5651c1bceac9ef6c7e95d4c6e838493338
zloivan <ivan.khomichkov@gmail.com>
2016-09-06 15:27:01 +0300

    A few cosmetic changes through out the code.

---
+++ Deferred Operators/MyVowelToConsonantRatioComparer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deferred_Operators
+{
+    public class MyVowelToConsonantRatioComparer : IComparer<string>
+    {
+
+        public int Compare(string x, string y)
+        {
+            int vCount1 = 0;
+            int cCount1 = 0;
+            int vCount2 = 0;
+            int cCount2 = 0;
+
+            GetVowelConsonantCount(x, ref vCount1, ref cCount1);
+            GetVowelConsonantCount(y, ref vCount2, ref cCount2);
+
+            double dRatio1 = (double)vCount1 / (double)cCount1;
+            double dRatio2 = (double)vCount2 / (double)cCount2;
+            if (dRatio1 < dRatio2)
+                return (-1);
+            else if (dRatio1 > dRatio2)
+                return (1);
+            else return (0);
+        }
+
+        //This method is public so our code using this comparer can get the values if it wants.
+        public void GetVowelConsonantCount(string s, ref int vowelCount, ref int consonantCount)
+        {
+            string vowels = "AEIOUY";
+
+            //Initialize the counts.
+            vowelCount = 0;
+            consonantCount = 0;
+
+            //Convert to uppercase so we are case insensitive
+            string sUpper = s.ToUpper();
+
+            foreach (char item in sUpper)
+            {
+                if (vowels.IndexOf(item) < 0)
+                    consonantCount++;
+                else
+                    vowelCount++;
+            }
+            return;
+        }
+    }
+}
--- Deferred Operators/Program.cs
+++ Deferred Operators/Program.cs
@@ -16,8 +16,11 @@ namespace Deferred_Operators
 
             ArrayList MyList = Employee.GetEmplyeesArrayList();
 
+            Employee[] employeeArray = Employee.GetEmploeesArray();
+
+            EmployeeOptionEntry[] employeeOptionArray = EmployeeOptionEntry.GetEmployeeOptionEntries();
             #region Where
-            
+
             //"Where" example by first name and by intex of our ArrayList
             //Where(this IEnumerable<T>, Func<T,bool>)
             //Where(this IEnumerable<T>, Func<T,bool>, int)
@@ -89,9 +92,7 @@ namespace Deferred_Operators
             }
 
             //Second Example with employee and employeeOption (complicated)
-            Employee[] employeeArray = Employee.GetEmploeesArray();
-
-            EmployeeOptionEntry[] employeeOptionArray = EmployeeOptionEntry.GetEmployeeOptionEntries();
+            
             
             //для каждого элемента из employeeArray вабрать несколько анонимных сущньстей по принципу:
             //Мы находим все элементы employeeOption у которых совпадает id с данным элементом employeeArray
@@ -344,8 +345,7 @@ namespace Deferred_Operators
                 Console.WriteLine(item);
             }
             #endregion
-            //Hello here is some code
-            //Are u sure there is Noany desiase about that  
+            
             #region GroupJoin
             //Example of a GroupJoin() operator.
             //GroupJoin(this Ienumerable<T>, IEnumerable<U>, Func<T,K>, Func<U,K>, Func<T, IEnumerable<U>, V>)
@@ -411,51 +411,8 @@ namespace Deferred_Operators
 
             #endregion
 
-        }
-    }
-    //OrderBy class for example.
-    public class MyVowelToConsonantRatioComparer : IComparer<string>
-    {
 
-        public int Compare(string x, string y)
-        {
-            int vCount1 = 0;
-            int cCount1 = 0;
-            int vCount2 = 0;
-            int cCount2 = 0;
 
-            GetVowelConsonantCount(x, ref vCount1, ref cCount1);
-            GetVowelConsonantCount(y, ref vCount2, ref cCount2);
-
-            double dRatio1 = (double)vCount1 / (double)cCount1;
-            double dRatio2 = (double)vCount2 / (double)cCount2;
-            if (dRatio1 < dRatio2)
-                return (-1);
-            else if (dRatio1 > dRatio2)
-                return (1);
-            else return (0);
-        }
-
-        //This method is public so our code using this comparer can get the values if it wants.
-        public void GetVowelConsonantCount(string s, ref int vowelCount, ref int consonantCount)
-        {
-            string vowels = "AEIOUY";
-
-            //Initialize the counts.
-            vowelCount = 0;
-            consonantCount = 0;
-
-            //Convert to uppercase so we are case insensitive
-            string sUpper = s.ToUpper();
-
-            foreach (char item in sUpper)
-            {
-                if (vowels.IndexOf(item) < 0)
-                    consonantCount++;
-                else
-                    vowelCount++;
-            }
-            return;
         }
     }
     
